def64ed55a9bd1e7de3210303239c9755ee6f667
MatMe07 <memat0403@gmail.com>
2025-09-16 13:39:15 +0300

    1 изменение

--- 3ISIP223_Pogosyan/Program.cs
+++ 3ISIP223_Pogosyan/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP223_Pogosyan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+        }
+    }
+
+    class Product
+    {
+        private int ID;
+        public string Name;
+        public double Price;
+        public int Count;
+        private bool HaveStock;
+        public string Categoric;
+    }
+}
