01f14b4ad2734f5a6ae795a2a193a80f76984952
MatMe07 <memat0403@gmail.com>
2025-09-17 14:20:33 +0300

    создал класс

--- 3ISIP223_Pogosyan/Program.cs
+++ 3ISIP223_Pogosyan/Program.cs
@@ -12,8 +12,87 @@ namespace _3ISIP223_Pogosyan
 
         static void Main(string[] args)
         {
+            //string txt = Console.ReadLine();
+            int slov = 0;
+            int n = 0;
+            do
+            {
+                Console.Clear();
+                Console.Write("Выберите действие:\n\n1. Ввод нового текста\n2. Вывод количества слов в тексте.\n3. Поиск самого короткого слова\n4. Подсчёт количества предложений\n5. Подсчёт количества гласных и согласных букв\n6. Поиск самого длинного слова\n7. Создание статистики по частоте встречаемости каждой буквы\n0. Выйти из программы\n\nВведите номер команды: ");
+                n = Convert.ToInt32(Console.ReadLine());
 
-            
+                switch (n) 
+                {
+                    
+                }
+
+
+            }
+            while (n != 0);
+
+        }
+
+        class Text
+        {
+            public string text;
+            public int countWords;
+            public string shortWord;
+            public int countSentences;
+            public int countGlasnie;
+            public int countSoglasnie;
+            public string longWord;
+            public Dictionary<char, int > letterMap;
+
+
+            public Text(string text)
+            {
+                this.text = text;
+                Stat();
+            }
+
+            public void Stat()
+            {
+                countWords = 1;
+                string znakKonza = ".?!";
+                string word = "";
+                string[] abzac = word.Split(new char[] { '\n' });
+
+                foreach (string predl in abzac) { 
+                    for (int i = 0; i < predl.Length; i++)
+                    {
+                        if (text[i] == ' ')
+                        {
+                            countWords++;
+                            if (countWords == 2) shortWord = word;
+                            else
+                            {
+                                if (word.Length < shortWord.Length)
+                                {
+                                    shortWord = word;
+                                }
+                                 word = "";
+                            }
+                        }
+                        else
+                        {
+                            word += text[i];
+                        }
+
+
+                        foreach (char c in znakKonza)
+                        {
+                            if (c == predl[i])
+                            {
+
+                            }
+                        }
+
+                    }
+
+                }
+                
+                }
+                
         }
 
     }
