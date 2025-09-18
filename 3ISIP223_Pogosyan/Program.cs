0713dca1505977c305f656326cd94142ff550d2c
MatMe07 <memat0403@gmail.com>
2025-09-18 14:13:37 +0300

    сделал 8 пункт

--- 3ISIP223_Pogosyan/Program.cs
+++ 3ISIP223_Pogosyan/Program.cs
@@ -16,7 +16,7 @@ namespace _3ISIP223_Pogosyan
             //string txt = Console.ReadLine();
             int slov = 0;
             int n = 0;
-            string str = "";
+            string str = " ";
             Text txt = new Text(str);
 
             do
@@ -47,7 +47,7 @@ namespace _3ISIP223_Pogosyan
                         }
                     case 4:
                         {
-                            Console.WriteLine($"Количество предложений в тексте = {txt.shortWord}");
+                            Console.WriteLine($"Количество предложений в тексте = {txt.countSentences}");
                             break;
                         }
                     case 5:
@@ -61,7 +61,49 @@ namespace _3ISIP223_Pogosyan
                             Console.WriteLine($"Самое длинное слово в тексте = {txt.longWord};");
                             break;
                         }
+                    case 7:
+                        {
+                            Console.WriteLine($"Cтатистика по частоте встречаемости каждой буквы: ");
+                            foreach (KeyValuePair<string, int> ent in txt.letterMap)
+                            {
+                                Console.WriteLine($"{ent.Key} = {ent.Value}");
+                            }
+                            break;
+                        }
+                    case 8:
+                        {
+                            Console.WriteLine("Прошлые тексты:");
+                            for (int i = 0; i < lstText.Count; i++)
+                            {
+                                Console.WriteLine($"{i+1}. {lstText[i].FirstWord}...");
+                            }
+                            Console.Write($"Выберите текст: ");
+                            int d = Convert.ToInt32( Console.ReadLine() ); d--;
+                            if (d > lstText.Count)
+                            {
+                                Console.WriteLine("Некорректный ввод данных!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine($"Количество слов в тексте = {lstText[d].countWords}");
+                                Console.WriteLine($"Самое короткое слово в тексте = {lstText[d].shortWord}");
+                                Console.WriteLine($"Количество предложений в тексте = {lstText[d].countSentences}");
+                                Console.WriteLine($"Количество гласных в тексте = {lstText[d].countGlasnie};");
+                                Console.WriteLine($"Количество согласных в тексте = {lstText[d].countSoglasnie};");
+                                Console.WriteLine($"Самое длинное слово в тексте = {lstText[d].longWord};");
+                                Console.WriteLine($"Cтатистика по частоте встречаемости каждой буквы: ");
+                                foreach (KeyValuePair<string, int> ent in lstText[d].letterMap)
+                                {
+                                    Console.WriteLine($"{ent.Key} = {ent.Value}");
+                                }
+
+                            }
+                            break;
+                        }
                 }
+                Console.Write("Нажмите Enter!");
+                Console.ReadLine();
 
 
             }
@@ -78,6 +120,7 @@ namespace _3ISIP223_Pogosyan
             public int countGlasnie;
             public int countSoglasnie;
             public string longWord;
+            public string FirstWord;
             public Dictionary<string, int> letterMap;
 
             public Text(string text)
@@ -102,15 +145,16 @@ namespace _3ISIP223_Pogosyan
                     if (text[i] == ' ')
                     {
                         countWords++;
+                        
                         //Console.WriteLine(word);
-                        if (countWords == 2) { shortWord = word; longWord = word; }
+                        if (countWords == 2) { shortWord = word; longWord = word; FirstWord = word; }
                         else
                         {
-                            if (word.Length < shortWord.Length )
+                            if (word.Length > 0 && word.Length < shortWord.Length )
                             {
                                 shortWord = word;
                             }
-                            if (word.Length > longWord.Length )
+                            if (word.Length > 0 && word.Length > longWord.Length )
                             {
                                 longWord = word;
                             }
@@ -120,7 +164,7 @@ namespace _3ISIP223_Pogosyan
                     else
                     {
                         simvol =  text[i].ToString();
-                        if (!Regex.IsMatch(simvol, "[.,:;?!-]")) { 
+                        if (!Regex.IsMatch(simvol, "[.,:;?!\\-\"]") && simvol != "\\") { 
                             word += text[i]; 
                             if (!letterMap.ContainsKey(simvol.ToLower()))
                             {
@@ -130,24 +174,16 @@ namespace _3ISIP223_Pogosyan
                             {
                                 letterMap[simvol.ToLower()]++;
                             }
-                            if (Regex.IsMatch(simvol.ToLower(), "[аоуэиыеёюя]")) countGlasnie++;
-                            if (Regex.IsMatch(simvol.ToLower(), "[бвгджзйклмнпрстфхцчшщ]")) countSoglasnie++;
+                            if (Regex.IsMatch(simvol.ToLower(), "[аоуэиыеёюiя]")) countGlasnie++;
+                            if (Regex.IsMatch(simvol.ToLower(), "[a-zа-я]")) countSoglasnie++;
                         }
                         if (Regex.IsMatch(simvol, "[.?!]") && text[i - 1].ToString().ToUpper() != text[i - 1].ToString()) countSentences++;
 
                     }
 
                 }
-                Console.WriteLine(countSentences);
-                Console.WriteLine(countGlasnie);
-                Console.WriteLine(countSoglasnie);
-                foreach(KeyValuePair<string, int> ent in letterMap)
-                {
-                    Console.WriteLine($"{ent.Key} = {ent.Value}");
-                }
 
-                
-                
+                countSoglasnie -= countGlasnie;
                 }
                 
         }
