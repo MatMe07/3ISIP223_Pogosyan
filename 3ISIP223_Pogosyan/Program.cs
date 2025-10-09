8838557da960aa61028ea858779b8a22fbdac25c
MatMe07 <memat0403@gmail.com>
2025-10-09 14:16:08 +0300

    добавлена защита нет

--- 3ISIP223_Pogosyan/Program.cs
+++ 3ISIP223_Pogosyan/Program.cs
@@ -53,6 +53,8 @@ namespace _3ISIP223_Pogosyan
         public double HP { get; set; }
         public double Attack { get; set; }
         public double Defense { get; set; }
+        public bool HaveDefense => Defense > 0;
+        public bool IsAlive => HP > 0;
         public Enemy(string name, double attack, double defense) 
         {
             HP = 100.0;
@@ -149,42 +151,108 @@ namespace _3ISIP223_Pogosyan
             string name = "Name";
             player = new Player(name);
         }
+
+        public void InforEnemAndPlayer(Enemy vrag)
+        {
+            if (vrag.IsAlive)
+            {
+                Console.WriteLine($"PlayerHp: {player.HP}, PlayerArmor: {player.Armor}  |  Vrag_name: {vrag.Name}, VragHp: {vrag.HP}, Zashita: {vrag.Defense}");
+            }
+            else
+            {
+                Console.WriteLine($"PlayerHp: {player.HP}, PlayerArmor: {player.Armor}  |  Vrag_name: {vrag.Name} -- Umer");
+            }
+        }
+
         public void GameStart()
         {
             int n = 0;
+            int RandEnemy = 0;
+            bool HaveEnemy = false;
+            Enemy vrag = null;
+            int boss = 1;
             while (true)
             {
-                Console.WriteLine();
-                n = Convert.ToInt32(Console.ReadLine());
-                switch (n)
-                {
-                    case 0:
-                        {
-                            break;
-                        }
-                    case 1:
-                        {
-
-                            break;
-                        }
-                    case 2:
-                        {
-                            break;
-                        }
-                    default:
-                        {
-                            break;
-                        }
-                }
-
+                Console.Clear();
+                InforEnemAndPlayer(vrag);
                 if (ChoiceChestOrEnemy) // Enemy
                 {
 
+                    if (!HaveEnemy)
+                    {
+                        RandEnemy = random.Next(0, 3);
+                        switch (RandEnemy)
+                        {
+                            case 0:
+                                {
+                                    vrag = new Skeleton();
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    vrag = new Magician();
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    vrag = new Goblin();
+                                    break;
+                                }
+                        }
+                        HaveEnemy = true;
+                        Console.Clear();
+                        InforEnemAndPlayer(vrag);
+                    }
+                    
+                    Console.Write("viberi!: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    switch (n)
+                    {
+                        case 0:
+                            {
+
+                                break;
+                            }
+                        case 1://attack
+                            {
+                                if (vrag.HaveDefense)
+                                {
+                                    vrag.Defense -= player.AttackWeapon;
+                                    vrag.Defense = vrag.HaveDefense ? vrag.Defense : 0;
+                                }
+                                else
+                                {
+                                    vrag.HP -= player.AttackWeapon;
+                                    vrag.HP = vrag.IsAlive ? vrag.HP : 0;
+                                }
+                                Console.Clear();
+                                InforEnemAndPlayer(vrag);
+                                break;
+                            }
+                        case 2://Defence
+                            {
+
+                                break;
+
+                            }
+                        default:
+                            {
+
+                                break;
+                            }
+                    }
+
+
+
                 }
+
                 else // Chest
                 {
                     ChestChoice();
                 }
+
+
+                boss++;
             }
         }
 
@@ -210,13 +278,13 @@ namespace _3ISIP223_Pogosyan
                         char choice;
                         while (true)
                         {
-                            choice = Convert.ToChar(Console.ReadLine());
-                            if (choice == 'y' || choice == 'Y' || choice == 'N' || choice == 'n')
+                            choice = Convert.ToChar(Console.ReadLine().ToLower());
+                            if (choice == 'y' || choice == 'n')
                             {
                                 break;
                             }
                         }
-                        if (choice == 'y' || choice == 'Y')
+                        if (choice == 'y')
                         {
                             player.AttackWeapon = RandAttackWeapon;
                         }
@@ -245,6 +313,10 @@ namespace _3ISIP223_Pogosyan
                         break;
                     }
 
+                default:
+                    {
+                        break;
+                    }
             }
         }
     }
