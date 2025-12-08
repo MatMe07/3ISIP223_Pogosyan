015f4be4783cb5c7a157671c0e9c3f59912cb85c
MatMe07 <arturpogc@gmail.com>
2025-12-08 20:37:47 +0300

    Созадны все страницы

---
+++ 3ISIP223_PogosyanWPF/Pages/ModelAndTypeEngine.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_PogosyanWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ModelAndTypeEngine.xaml
+    /// </summary>
+    public partial class ModelAndTypeEngine : Page
+    {
+        public ModelAndTypeEngine()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new KreditParameters());
+        }
+    }
+}
