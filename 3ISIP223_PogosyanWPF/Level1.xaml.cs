d76dbf7071f66d27638787fa1576bf42368bccca
MatMe07 <arturpogc@gmail.com>
2026-03-10 23:54:43 +0300

    распределил по отдльным файлам интерфейс

---
+++ 3ISIP223_PogosyanWPF/Level1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_PogosyanWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Level1.xaml
+    /// </summary>
+    public partial class Level1 : Page
+    {
+        public Level1()
+        {
+            InitializeComponent();
+        }
+
+        private void ModelUIElement3D_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
