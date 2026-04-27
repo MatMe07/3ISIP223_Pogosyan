46dcef25595d9240b6f6190b556785a45bba86e7
MatMe07 <arturpogc@gmail.com>
2026-04-27 23:21:19 +0300

    реализвал slidebar, и страницу каталог книг

---
+++ 3ISIP223_PogosyanWPF/Pages/CatalogPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_PogosyanWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CatalogPage.xaml
+    /// </summary>
+    public partial class CatalogPage : Page
+    {
+
+        public List<string> lst {  get; set; }
+        public CatalogPage()
+        {
+            lst = new List<string> { "hello", "world" };
+            DataContext = this;
+            InitializeComponent();
+            //listB.ItemsSource = lst;
+        }
+
+        private void AddToListButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            //var bookId = (int)button.Tag;
+
+            var contextMenu = FindResource("ReadingListMenu") as ContextMenu;
+            contextMenu.PlacementTarget = button;
+            contextMenu.IsOpen = true;
+            foreach (MenuItem item in contextMenu.Items)
+            {
+                //item.Click -= ReadingListItem_Click;
+                item.Click += ReadingListItem_Click;
+                //item.Tag = bookId;
+            }
+        }
+
+        private void ReadingListItem_Click(object sender, RoutedEventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            //int bookId = (int)menuItem.Tag;
+            string status = menuItem.Header.ToString();
+
+            //AddBookToReadingList(bookId, status);
+
+            (menuItem.Parent as ContextMenu).IsOpen = false;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            var res = MessageBox.Show("Книга {bookId} → {status}");
+            //if (res == MessageBoxResult.OK) listBooks.SelectedIndex = -1;
+
+        }
+    }
+}
