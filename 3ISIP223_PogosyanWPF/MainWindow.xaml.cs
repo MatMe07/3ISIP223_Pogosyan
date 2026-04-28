eddc1ea008e5cd346c47f983001c4ad2f2689cf7
MatMe07 <arturpogc@gmail.com>
2026-04-29 00:29:00 +0300

    расположение кнопки добавить в список

--- 3ISIP223_PogosyanWPF/MainWindow.xaml.cs
+++ 3ISIP223_PogosyanWPF/MainWindow.xaml.cs
@@ -26,6 +26,11 @@ namespace _3ISIP223_PogosyanWPF
             //var wind = new LoginRegisterWindows();
             //var res = wind.ShowDialog();
             //if (res != false) this.Close();
+            var wind = new BookWindow();
+            var res = wind.ShowDialog();
+            if (res != false) this.Close();
+
+
             InitializeComponent();
         }
     }
