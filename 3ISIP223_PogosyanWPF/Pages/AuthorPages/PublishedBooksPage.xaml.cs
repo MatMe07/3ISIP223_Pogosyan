2f3679c420b26f673c6ff58cea5a651be74d822e
MatMe07 <arturpogc@gmail.com>
2026-05-04 23:52:29 +0300

    реализовал кладку автора.

---
+++ 3ISIP223_PogosyanWPF/Pages/AuthorPages/PublishedBooksPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_PogosyanWPF.Pages.AuthorPages
+{
+    /// <summary>
+    /// Логика взаимодействия для PublishedBooksPage.xaml
+    /// </summary>
+    public partial class PublishedBooksPage : Page
+    {
+        public List<string> lstRand { get; set; }
+
+        public PublishedBooksPage()
+        {
+            lstRand = new List<string> { "fdf", "sdf" };
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void btnEditBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
